a3eccae1dd6950b410d47b1981dfa7453d9aa5e1
Hardik <prajapatihardik2910@gmail.com>
2022-07-11 18:39:13 +0530

    some changes i made

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { IconButton, InputBase } from "@mui/material";
-import { PhotoCamera } from "@mui/icons-material";
+import { Delete, PhotoCamera } from "@mui/icons-material";
 
 function App() {
-  return (
-    <>
-      <Button
-        variant="contained"
-        onClick={() => {
-          alert("clicked");
-        }}
-        size="small"
-        color="success"
-      >
-        Hello World
-      </Button>
-
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span">
-          Upload
-        </Button>
-      </label>
-      <label htmlFor="icon-button-file">
-        <InputBase accept="image/*" id="icon-button-file" type="file" />
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="span"
-        >
-          <PhotoCamera />
-        </IconButton>
-      </label>
-    </>
-  );
+  return <>hello</>;
 }
 export default App;
